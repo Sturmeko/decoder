e3870d45fb4db999e0b697777829a2a8bf23656d
Aleksejs Sturme-Kostarnovs <aleks.co@hotmail.com>
2021-07-09 15:25:31 +0300

    Fix error with missing letters

--- decoder.rb
+++ decoder.rb
@@ -8,7 +8,9 @@ def decode(numbers)
   numbers.each do |number|
     # Get the letter that corresponds to this number.
     letter = KEY[number]
-    # Add it onto the string.
+    # If letter was found...
+    if letter != nil
+      # Add it onto the string.
     text += letter
   end
   # Return the decoded string.
